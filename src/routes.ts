860ab98e17982188d12110bf1656407c85d1ca5a
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-10-21 23:44:11 -0300

    Add - Rota para cadastro de restaurantes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import multer from 'multer'
 
 import uploadConfig from './config/upload'
+import RestaurantControllers from './controllers/RestaurantControllers'
  
 const routes = Router()
 const upload = multer(uploadConfig)
@@ -10,44 +11,6 @@ routes.get('/', (req, res) => {
   return res.json({ status: 201 })
 })
 
-routes.post('/restaurants', upload.array('images'), (req, res) => {
-  
-  return res.json({
-    status:201,
-    data:{
-        name: 'Big Meat',
-      address: { 
-        street: 'Rua Burger',
-        number: 275,
-        complement: 'none',
-        state: 'São Paulo',
-        city: 'Sorocaba',
-        zip_code: '180.50-060'
-      },
-      images: [
-      '1.jpg',
-      '2.jpeg',    
-      '3.jpg',
-      ],
-      workingHours:[
-        { 
-          weekday: 'sexta',
-          openingTime: '11:30',
-          closingTime: '24:00'
-        },
-        { 
-          weekday: 'sábado',
-          openingTime: '15:30',
-          closingTime: '24:00'
-        },
-        { 
-          weekday: 'domingo',
-          openingTime: '15:30',
-          closingTime: '20:00'
-        },
-      ]
-    }
-  })
-})
+routes.post('/restaurants', upload.array('images'), RestaurantControllers.create)
 
 export default routes
