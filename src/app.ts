382a9cb37451beef820808430e225abd28a72077
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-10-23 00:07:44 -0300

    Add - Funcionalidade para o express retornar arquivos estaticos

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes'
+import path from 'path'
+
+const UPLOADS = path.join(__dirname, '..', 'uploads')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(routes)
+app.use('/uploads', express.static(UPLOADS))
 
 export default app
