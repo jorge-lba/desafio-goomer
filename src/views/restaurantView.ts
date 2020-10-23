2d4607f4514040afaaae7542ae7c4ca90071315c
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-10-23 01:27:12 -0300

    Add - Funcionalidade para retornar descriçao do horario de funcionamento como o exempo

--- src/views/restaurantView.ts
+++ src/views/restaurantView.ts
@@ -1,12 +1,39 @@
+/* eslint-disable camelcase */
 import { DataRestaurant } from '../@types/TypesRestaurant'
 import imagesView from '../views/imagesView'
 
 export default {
   render (restaurant: DataRestaurant) {
+    const working_hours = restaurant.working_hours
+
+    const formatHours = (hours:string) => {
+      const [hour, minute] = hours.split(':')
+
+      return minute !== '00' ? `${hour}h${minute}` : `${hour}h`
+    }
+
+    const res = working_hours.reduce((acc, current, index, array) => {
+      const {
+        weekday_end,
+        weekday_start,
+        opening_time,
+        closing_time
+      } = current
+
+      acc += `${weekday_start} à ${weekday_end} das ${formatHours(opening_time)} as ${formatHours(closing_time)}`
+
+      if (index + 1 < array.length) {
+        acc += ' e de '
+      }
+
+      return acc
+    }, 'De ')
+
     return {
       name: restaurant.name,
       address: restaurant.address,
       working_hours: restaurant.working_hours,
+      description_workings_hours: res,
       images: imagesView.renderMany(restaurant.images)
     }
   },
